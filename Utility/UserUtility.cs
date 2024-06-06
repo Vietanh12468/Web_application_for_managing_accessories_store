76cc7ff94e97901592e71762c596b534de09166f
Viet Anh <vietanh12468@yahoo.com>
2024-06-06 23:56:49 +0700

    A

--- Utility/UserUtility.cs
+++ Utility/UserUtility.cs
@@ -22,6 +22,12 @@ namespace Web_application_for_managing_accessories_store.Utility
         public async Task<List<User>> GetAsync() => await _userCollection.Find(_ => true).ToListAsync();
         public async Task<User> GetAsync(string Id) => await _userCollection.Find(x => x.Id == Id).FirstOrDefaultAsync();
 
+        public async Task<User> GetAsync<T>(string attribute, T value)
+        {
+            var filter = Builders<User>.Filter.Eq(attribute, value);
+            return await _userCollection.Find(filter).FirstOrDefaultAsync();
+        }
+
         public async Task CreateAsync(User user) => await _userCollection.InsertOneAsync(user);
         public async Task UpdateAsync(User user) => await _userCollection.ReplaceOneAsync(x => x.Id == user.Id, user);
         public async Task RemoveAsync(string Id) => await _userCollection.DeleteOneAsync(x => x.Id == Id);
